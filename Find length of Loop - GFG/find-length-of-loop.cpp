3a012fb2d0315281bc2e5adb63d43f4f2259e78d
Divvi satya sriram <111105934+Sriram-99@users.noreply.github.com>
2023-06-07 16:58:30 +0530

    Added solution - LeetHub

---
+++ Find length of Loop - GFG/find-length-of-loop.cpp
@@ -0,0 +1,113 @@
+//{ Driver Code Starts
+// driver code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+int countNodesinLoop(Node* head);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        cout<< countNodesinLoop(head) << endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+//Function to find the length of a loop in the linked list.
+int countNodesinLoop(struct Node *head)
+{
+    if(!head) return 0;
+    if(!head || !head->next) return 0;
+        Node*slow=head;
+        Node*fast=head;
+        int isloop=0;
+        do{
+            if(fast==NULL || fast->next==NULL) break;
+           
+            slow=slow->next;
+            fast=fast->next->next;
+            
+        }while(slow!=fast);
+       if(slow==fast) isloop=1;
+       if(!isloop)return 0;
+        slow=head;
+        if(slow==fast){
+            int cnt=1;
+            while(slow->next!=head){
+                cnt++;
+                slow=slow->next;
+            }
+            return cnt;
+        }
+        while(slow!=fast){
+            slow=slow->next;
+            fast=fast->next;
+        }
+        int cnt=1;
+        while(slow->next!=fast){
+            cnt++;
+            slow=slow->next;
+        }
+        return cnt;
+        
+}
