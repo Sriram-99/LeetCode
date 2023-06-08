c9619e8f19a040650eb0856c17fe4287c61dadaf
Divvi satya sriram <111105934+Sriram-99@users.noreply.github.com>
2023-06-08 18:02:41 +0530

    Added solution - LeetHub

--- Implement two stacks in an array - GFG/implement-two-stacks-in-an-array.cpp
+++ Implement two stacks in an array - GFG/implement-two-stacks-in-an-array.cpp
@@ -64,13 +64,9 @@ int main()
 //Function to push an integer into the stack1.
 void twoStacks :: push1(int x)
 {
-  if(top1+1==top2) return;
- 
-  else {
-       top1++;
-       arr[top1]=x;
-  }
-  
+    if(top1+1==top2)return;
+    top1++;
+    arr[top1]=x;
 }
    
 //Function to push an integer into the stack2.
@@ -84,19 +80,16 @@ void twoStacks ::push2(int x)
 //Function to remove an element from top of the stack1.
 int twoStacks ::pop1()
 {
- if(top1==-1) return -1;
- int temp=arr[top1];
- top1--;
- return temp;
+    if(top1==-1)return -1;
+      top1--;
+      return arr[top1+1];
 }
 
 //Function to remove an element from top of the stack2.
 int twoStacks :: pop2()
 {
-  if(top2==100) return -1;
-  int temp=arr[top2];
-  top2++;
-  return temp;
-   
+    if(top2==size)return -1;
+    top2++;
+    return arr[top2-1];
 }
 
