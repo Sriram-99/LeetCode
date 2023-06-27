017fc6568f75482dee2052908401df3329bb53fb
Divvi satya sriram <111105934+Sriram-99@users.noreply.github.com>
2023-06-27 17:27:12 +0530

    Added solution - LeetHub

--- Largest square formed in a matrix - GFG/largest-square-formed-in-a-matrix.cpp
+++ Largest square formed in a matrix - GFG/largest-square-formed-in-a-matrix.cpp
@@ -9,78 +9,27 @@ using namespace std;
 
 class Solution{
 public:
-    int solve(int n, int m, vector<vector<int>>&mat,int &ans,int i,int j
-    ,vector<vector<int>>&dp){
-        if(i>=n || j>=m ) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int right=solve(n,m,mat,ans,i,j+1,dp);
-        int down=solve(n,m,mat,ans,i+1,j,dp);
-        int diagonal=solve(n,m,mat,ans,i+1,j+1,dp);
-         if(mat[i][j]==0){
-            dp[i][j]=0;
-            return 0;
-        }
-        int val=min(right,min(down,diagonal));
-        ans=max(val+1,ans);
-        
-        dp[i][j]=val+1;
-        return val+1;
-    }
-    
-    int solveTab(int n, int m, vector<vector<int>> mat){
-        vector<vector<int>>dp(n,vector<int>(m,0));
-        int ans=0;
-        for(int i=n-1;i>=0;i--){
-            for( int j=m-1;j>=0;j--){
-                if(mat[i][j]==1){
-                    int right=0,down=0,diagonal=0;
-                if(i<n && j+1<m)
-                 right=dp[i][j+1];
-                 if(i+1<n && j<m)
-                 down=dp[i+1][j];
-                 if(i+1<n && j+1<m)
-                 diagonal=dp[i+1][j+1];
-                 int val=min(right,min(down,diagonal));
-                 ans=max(val+1,ans);
-                 dp[i][j]=val+1;
-                }
-                
-            }
-        }
-        return ans;
+
+int solve(vector<vector<int>>& MAT, int n, int m,int i,int j,int&ans,vector<vector<int>>& dp){
+    if(i>=n || j>=m) return 0;
+    if(dp[i][j]!=-1) return dp[i][j];
+    int right=solve(MAT,n,m,i,j+1,ans,dp);
+    int down=solve(MAT,n,m,i+1,j,ans,dp);
+    int diag=solve(MAT,n,m,i+1,j+1,ans,dp);
+    int val=0;
+    if(MAT[i][j]==1){
+      val=1+min(right,min(down,diag));
+        ans=max(ans,val);
         
     }
-    int solveSpace(int n,int m,vector<vector<int>>&mat){
-        vector<int>prev(m+1,0);
-        vector<int>curr(m+1,0);
-        int ans=0;
-       for(int i=n-1;i>=0;i--){
-            for( int j=m-1;j>=0;j--){
-                if(mat[i][j]==1){
-                    int right=0,down=0,diagonal=0;
-                if(i<n && j+1<m)
-                 right=curr[j+1];
-                 if(i+1<n && j<m)
-                 down=prev[j];
-                 if(i+1<n && j+1<m)
-                 diagonal=prev[j+1];
-                 int val=min(right,min(down,diagonal));
-                 ans=max(val+1,ans);
-                 curr[j]=val+1;
-                }
-                else{
-                    curr[j]=0;
-                }
-                
-            }
-            prev=curr;
-        }
-        return ans;
-    }
-    
-    
-    int maxSquare(int n, int m, vector<vector<int>> mat){
-      return solveSpace(n,m,mat);
+    dp[i][j]=val;
+     return dp[i][j];
+}
+    int maxSquare(int n, int m, vector<vector<int>>MAT){
+         int ans=0;
+   vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+   solve(MAT,n,m,0,0,ans,dp);
+   return ans;
     }
 };
 
