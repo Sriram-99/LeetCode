9b4beab26e631e74a371d1e97fb3b24729bc6cb7
Divvi satya sriram <111105934+Sriram-99@users.noreply.github.com>
2023-09-02 00:14:38 +0530

    Added solution - LeetHub

---
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    /*  Function to implement Bellman Ford
+    *   edges: vector of vectors which represents the graph
+    *   S: source vertex to start traversing graph with
+    *   V: number of vertices
+    */
+    vector<int> bellman_ford(int n, vector<vector<int>>& edges, int src) {
+        vector<int>dist(n,1e8);
+               dist[src]=0;
+               int m=edges.size();
+               for(int j=1;j<=n;j++){
+                   for(int i=0;i<m;i++){
+                       int u=edges[i][0];
+                       int v=edges[i][1];
+                       int wt=edges[i][2];
+                       if(dist[u]!=1e8 && dist[u]+wt<dist[v]){
+                           if(j==n) return {-1};
+                           dist[v]=dist[u]+wt;
+                       }
+                   }
+               }
+               return dist;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, m;
+        cin >> N >> m;
+        vector<vector<int>> edges;
+
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 3; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+
+        int src;
+        cin >> src;
+
+        Solution obj;
+        vector<int> res = obj.bellman_ford(N, edges, src);
+
+        for (auto x : res) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
